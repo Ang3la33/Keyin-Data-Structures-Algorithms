f8246a98827b38d9cdef656464a343481804068f
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-01-19 16:09:48 -0330

    Initial commit

---
+++ src/Assignment1/TempAboveAverage.java
@@ -0,0 +1,16 @@
+package Assignment1;
+
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class TempAboveAverage {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] tempArray = new int[5];
+
+
+
+    }
+}
