97be95e21c73fb2077be1e7ecd96646e04f882c0
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-01-19 18:14:25 -0330

    Assignment 1 complete: Takes input from user for array, calculates average, counts temperatures above average

--- src/Assignment1/TempAboveAverage.java
+++ src/Assignment1/TempAboveAverage.java
@@ -5,12 +5,56 @@ import java.util.Arrays;
 
 public class TempAboveAverage {
 
+    Scanner scanner = new Scanner(System.in);
+    double array[] = null;
+
+    public TempAboveAverage(int sizeOfArray) {
+        array = new double[sizeOfArray];
+        System.out.println("Enter " + sizeOfArray + " temperatures: ");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextDouble();
+        }
+    }
+
+    public double calculateAverage() {
+        double average = Arrays.stream(array).sum() / array.length;
+        return average;
+    }
+
+    public int countAboveAverage() {
+        int counter = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > calculateAverage()) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public String aboveAverageString() {
+        if (countAboveAverage() == 1) {
+            return "There is 1 temperature above average";
+        }
+        else {
+            return "There are " + countAboveAverage() + " temperatures above average";
+        }
+    }
+
+
+
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
-        int[] tempArray = new int[5];
+        System.out.println();
+        System.out.println("Average Temperature Calculator");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        System.out.println();
+        TempAboveAverage tempArray = new TempAboveAverage(5);
 
-
+        System.out.println();
+        System.out.println("The average temperature is " + tempArray.calculateAverage());
+        System.out.println();
+        System.out.println(tempArray.aboveAverageString());
 
     }
 }
